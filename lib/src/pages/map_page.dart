c34b516594f2551308a62a48e60c8759066b440e
julian <juliandanielfranco@gmail.com>
2021-05-30 09:33:08 -0500

    improve google map

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,9 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:qrscanner_v2/src/models/scan_model.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
+
   @override
   Widget build(BuildContext context) {
+
+
+    final CameraPosition initialPoint = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
 
     final ScanModel scanModel = ModalRoute.of(context).settings.arguments;
 
@@ -11,8 +29,12 @@ class MapPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Mapa"),
       ),
-      body: Center(
-        child: Text(scanModel.valor),
+      body:GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
